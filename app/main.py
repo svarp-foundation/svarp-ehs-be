6adceb05b4a57db21b98815750b3291d48de6831
DeveloperSwastik <developerswastiksharma@gmail.com>
2025-12-03 18:46:18 +0530

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.api.site import router as site_router
 from app.api.department import router as department_router
 from app.api.audit import router as audit_router
 from app.api.finding import router as finding_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="SVARP Backend")
 app.include_router(company_router)
@@ -20,7 +21,13 @@ app.include_router(site_router)
 app.include_router(department_router)
 app.include_router(audit_router)
 app.include_router(finding_router)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # During development allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 Base.metadata.create_all(bind=engine)
 
 @app.get("/")
